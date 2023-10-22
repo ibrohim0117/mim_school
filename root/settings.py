4fc779009346cbf293ca7cbfde87569cbafa341d
ibrohim0117 <ibrohim.dev.uz@gmail.com>
2023-10-22 16:19:40 +0500

    add static and update index.html

--- root/settings.py
+++ root/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -107,6 +108,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+
+# STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- root/urls.py
+++ root/urls.py
@@ -1,6 +1,12 @@
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from root import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('home.urls'))
+# ] + static(settings.STATIC_URL, docoment_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, docoment_root=settings.STATICFILES_DIRS) + static(settings.MEDIA_URL, docoment_root=settings.MEDIA_ROOT)
+
